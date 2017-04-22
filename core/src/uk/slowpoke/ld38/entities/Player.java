564602f72f855986b081b0c3154c99c0349de568
Mali <malii.coxon@gmail.com>
2017-04-22 22:38:55 +0100

    Added ship select to player.

--- core/src/uk/slowpoke/ld38/entities/Player.java
+++ core/src/uk/slowpoke/ld38/entities/Player.java
@@ -1,6 +1,9 @@
 package uk.slowpoke.ld38.entities;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Rectangle;
 
 // does not technically fall under the entity class but is an entity controller.
 
@@ -9,6 +12,8 @@ public class Player {
 	
 	String name;
 	Color colour;
+	
+	ArrayList<Unit> units = new ArrayList<Unit>();
 	
 	public Player(String name,Color colour){
 		this.name = name;
@@ -22,5 +27,23 @@ public class Player {
 	public String getName(){
 		return name;
 	}
+	
+	public void addUnit(int x,int y){
+		units.add(new Unit(x,y));
+	}
 
+	public ArrayList<Unit> getUnits(){
+		return units;
+	}
+
+	public void selectShips(Rectangle selection) {
+		for(Unit unit: units){
+			System.out.println(selection);
+			
+			if(selection.contains(unit.getX(),unit.getY())){
+				System.out.println("CONTAINS");
+			}
+			
+		}
+	}	
 }
