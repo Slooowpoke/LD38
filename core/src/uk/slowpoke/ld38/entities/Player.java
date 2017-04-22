afa631b0a59571952009a80e7b0ecbdf95d981d8
Mali <malii.coxon@gmail.com>
2017-04-22 20:58:26 +0100

    Changed how colour is spelt and added getter.

--- core/src/uk/slowpoke/ld38/entities/Player.java
+++ core/src/uk/slowpoke/ld38/entities/Player.java
@@ -7,13 +7,16 @@ import com.badlogic.gdx.graphics.Color;
 public class Player {
 	// this will store your choices after each day.
 	
-	
 	String name;
-	Color color;
+	Color colour;
 	
-	public Player(String name,Color color){
+	public Player(String name,Color colour){
 		this.name = name;
-		this.color = color;
+		this.colour = colour;
+	}
+	
+	public Color getColour(){
+		return colour;
 	}
 
 	public String getName(){
