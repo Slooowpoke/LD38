e12ee2f93d38773cc058adb753b29f22982b84e8
Mali <malii.coxon@gmail.com>
2017-04-22 20:57:43 +0100

    Added camera movement.

--- core/src/uk/slowpoke/ld38/Game.java
+++ core/src/uk/slowpoke/ld38/Game.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
 
 import uk.slowpoke.ld38.entities.Player;
@@ -20,23 +22,34 @@ import uk.slowpoke.ld38.world.Map;
 public class Game extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
 	ShapeRenderer shapeBatch;
+	
+	// CAMERA VARIABLES --
+	
 	OrthographicCamera camera;
+	int x,y;
+	boolean up,down,left,right;
 	float zoomTarget = 0.5f;
+	
+	// --
 	
 	Map map;
 	
-	ArrayList<Player> players = new ArrayList<Player>();
-	int x,y;
+	public static ArrayList<Player> players = new ArrayList<Player>();
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		shapeBatch = new ShapeRenderer();
-		map = new Map();
 		
-		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		players.add(new Player("Mali",new Color(Color.RED)));
+		players.add(new Player("BlueTeam",new Color(Color.BLUE)));
+		map = new Map();
+		camera = new OrthographicCamera(30, 30 * (Gdx.graphics.getHeight() / Gdx.graphics.getWidth()));
+//		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.input.setInputProcessor(this);
+
 	} 
 
 	@Override
@@ -52,10 +65,30 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 		shapeBatch.end();
 	}
 	
+
 	public void update(float d){
+		
+		// all camera shizzle
 		float lerp = 0.05f;
 		Vector3 position = camera.position;
+		float dx = 0, dy = 0,speed = 3f;
+		speed *= (camera.zoom*10)/2f;
+		
+		if(up){
+			dy += speed;
+		}else if(down){
+			dy -= speed;
+		}
+		if(left){
+			dx -= speed;
+		}else if(right){
 
+			dx += speed;
+		}
+		x += dx*(d*50f);
+		y += dy*(d*50f);
+		
+		
 		camera.position.x += (x - position.x) * lerp;
 		camera.position.y += (y - position.y) * lerp;
 
@@ -63,11 +96,19 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 //		camera.position.y += ((camera.position.y+shakeY)-camera.position.y)*0.1;
 
 //		if(!map.isHover()){
-//			camera.position.x = MathUtils.clamp(camera.position.x, Gdx.graphics.getWidth()/4, 70*16-Gdx.graphics.getWidth()/4);
-//			camera.position.y = MathUtils.clamp(camera.position.y, (6*16)+Gdx.graphics.getWidth()/4, (82*16)-Gdx.graphics.getWidth()/4);
+//			camera.position.x = MathUtils.clamp(camera.position.x, Gdx.graphics.getWidth()/2, 100);
+//			camera.position.y = MathUtils.clamp(camera.position.y, Gdx.graphics.getHeight()/2, 100);
 //		}
+		
 		camera.zoom += (zoomTarget-camera.zoom)*0.1f;
+//		camera.zoom = MathUtils.clamp(camera.zoom, 0.1f, 100/camera.viewportWidth);
 		camera.update();
+//		float effectiveViewportWidth = camera.viewportWidth * camera.zoom;
+//		float effectiveViewportHeight = camera.viewportHeight * camera.zoom;
+
+//		camera.position.x = MathUtils.clamp(camera.position.x, effectiveViewportWidth / 2, 800 - effectiveViewportWidth / 2f);
+//		camera.position.y = MathUtils.clamp(camera.position.y, effectiveViewportHeight / 2, 600 - effectiveViewportHeight / 2f);
+
 	}
 	
 	@Override
@@ -79,16 +120,40 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 	@Override
 	public boolean keyDown(int key) {
 		if(key == Keys.A || key == Keys.LEFT){
-			
+			left = true;
+		}
+		if(key == Keys.D || key == Keys.RIGHT){
+			right = true;
+		}
+		if(key == Keys.S || key == Keys.DOWN){
+			down = true;
+		}
+		if(key == Keys.W || key == Keys.UP){
+			up = true;
 		}
 		return false;
 	}
 
 	@Override
-	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
+	public boolean keyUp(int key) {
+		if(key == Keys.A || key == Keys.LEFT){
+			left = false;
+		}
+		if(key == Keys.D || key == Keys.RIGHT){
+			right = false;
+		}
+		if(key == Keys.S || key == Keys.DOWN){
+			down = false;
+		}
+		if(key == Keys.W || key == Keys.UP){
+			up = false;
+		}
+
+		
 		return false;
 	}
+	
+	
 
 	@Override
 	public boolean keyTyped(char character) {
