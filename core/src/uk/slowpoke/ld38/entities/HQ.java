60167b2ffd66489b24ac806c5e7a4de394818e59
Mali <malii.coxon@gmail.com>
2017-04-23 23:53:33 +0100

    Added HQ class.

---
+++ core/src/uk/slowpoke/ld38/entities/HQ.java
@@ -0,0 +1,12 @@
+package uk.slowpoke.ld38.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class HQ extends LandEntity{
+
+	public HQ(float x, float y,Player owner) {
+		super(x, y, 20, owner,new Sprite(new Texture("HQ.png")));
+	}
+}
