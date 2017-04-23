40dc47fc0a51da57c92b8a4f6621118f0cc31a1c
Mali <malii.coxon@gmail.com>
2017-04-23 23:54:06 +0100

    Added Scout class.

---
+++ core/src/uk/slowpoke/ld38/entities/Scout.java
@@ -0,0 +1,12 @@
+package uk.slowpoke.ld38.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Scout extends AirEntity{
+
+	public Scout(float x, float y,Player owner) {
+		super(x, y, 20, owner,new Sprite(new Texture("scout.png")));
+	}
+}
