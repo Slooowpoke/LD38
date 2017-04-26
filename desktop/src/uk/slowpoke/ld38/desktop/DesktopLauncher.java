3f8ef3a6b067ac2976cd8f4cc95e4ffa4f670b3c
Mali <malii.coxon@gmail.com>
2017-04-27 00:56:07 +0100

    Reduced window size, stopped resizing.

--- desktop/src/uk/slowpoke/ld38/desktop/DesktopLauncher.java
+++ desktop/src/uk/slowpoke/ld38/desktop/DesktopLauncher.java
@@ -7,9 +7,10 @@ import uk.slowpoke.ld38.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 600;
-		
+		config.width = 400;
+		config.height = 400;
+		config.resizable = false;
+
 		new LwjglApplication(new Game(), config);
 	}
 }
